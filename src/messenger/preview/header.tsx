1dbddec61202bace371f4f20acb84389a34a1aa1
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-22 16:18:04 +0800

    feat: preview top section done

--- src/messenger/preview/header.tsx
+++ src/messenger/preview/header.tsx
@@ -4,19 +4,22 @@ import classNames from 'classnames';
 const Header: React.FC = () => (
   <div className={classNames('messenger-preview-header-container')}>
     <section>
-      <img alt="me" src={`${process.env.PUBLIC_URL}/avatars/000.jpeg`} />
+      <img alt="me" src={`${process.env.PUBLIC_URL}/avatars/000.jpg`} />
       <h3>聊天室</h3>
       <button type="button">
+        <img alt="setting" src={`${process.env.PUBLIC_URL}/icons/3dots.png`} />
+      </button>
+      <button type="button">
         <img
-          alt="setting"
-          src={`${process.env.PUBLIC_URL}/icons/setting.png`}
+          alt="video call"
+          src={`${process.env.PUBLIC_URL}/icons/video-call.png`}
         />
       </button>
       <button type="button">
-        <img alt="video" src={`${process.env.PUBLIC_URL}/icons/video.png`} />
-      </button>
-      <button type="button">
-        <img alt="write" src={`${process.env.PUBLIC_URL}/icons/write.png`} />
+        <img
+          alt="new message"
+          src={`${process.env.PUBLIC_URL}/icons/new-message.png`}
+        />
       </button>
     </section>
     <input />
