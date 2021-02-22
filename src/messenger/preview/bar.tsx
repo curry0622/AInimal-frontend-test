5cfe211da04d4c00127a4f4eedfadf5ab5625460
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-22 18:43:03 +0800

    feat: calculate time diff

--- src/messenger/preview/bar.tsx
+++ src/messenger/preview/bar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
+import dayjs from 'dayjs';
 
 interface IProps {
   id: number;
@@ -13,17 +14,44 @@ const Bar: React.FC<IProps> = ({
   nickname,
   last_message,
   last_message_timestamp,
-}) => (
-  <div className={classNames('messenger-preview-content-bar-container')}>
-    <img alt="avatar" src={`${process.env.PUBLIC_URL}/avatars/${id}.jpg`} />
-    <section>
-      <h4>{nickname}</h4>
-      <div>
-        <span>{last_message} </span>
-        <span> {last_message_timestamp}</span>
-      </div>
-    </section>
-  </div>
-);
+}) => {
+  const calTimeDiff = () => {
+    if (dayjs().diff(last_message_timestamp, 'second') < 60) {
+      return ` · ${dayjs().diff(last_message_timestamp, 'second')}秒`;
+    }
+    if (dayjs().diff(last_message_timestamp, 'minute') < 60) {
+      return ` · ${dayjs().diff(last_message_timestamp, 'minute')}分鐘`;
+    }
+    if (dayjs().diff(last_message_timestamp, 'hour') < 24) {
+      return ` · ${dayjs().diff(last_message_timestamp, 'hour')}小時`;
+    }
+    if (dayjs().diff(last_message_timestamp, 'day') < 7) {
+      return ` · ${dayjs().diff(last_message_timestamp, 'day')}天`;
+    }
+    if (dayjs().diff(last_message_timestamp, 'week') < 5) {
+      return ` · ${dayjs().diff(last_message_timestamp, 'week')}星期`;
+    }
+    if (dayjs().diff(last_message_timestamp, 'month') < 12) {
+      return ` · ${dayjs().diff(last_message_timestamp, 'month')}個月`;
+    }
+    if (dayjs().diff(last_message_timestamp, 'year') < 12) {
+      return ` · ${dayjs().diff(last_message_timestamp, 'year')}年前`;
+    }
+    return '';
+  };
+
+  return (
+    <div className={classNames('messenger-preview-content-bar-container')}>
+      <img alt="avatar" src={`${process.env.PUBLIC_URL}/avatars/${id}.jpg`} />
+      <section>
+        <h4>{nickname}</h4>
+        <div>
+          <span>{last_message} </span>
+          <span> {calTimeDiff()}</span>
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default Bar;
