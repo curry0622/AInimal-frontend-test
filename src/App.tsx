e11779edec791df5281abf31880d23948fe1472e
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-22 13:16:58 +0800

    feat: add basic scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">ainimal</div>
-  );
+  return <div className="App">ainimal</div>;
 }
 
 export default App;
