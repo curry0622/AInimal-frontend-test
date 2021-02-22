2535bed13f43301bcd5ea83ba7c35a978c71ce6c
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-22 15:52:33 +0800

    feat: preview header top section layout

--- src/messenger/preview/header.tsx
+++ src/messenger/preview/header.tsx
@@ -2,7 +2,25 @@ import React from 'react';
 import classNames from 'classnames';
 
 const Header: React.FC = () => (
-  <div className={classNames('messenger-preview-header-container')}>Header</div>
+  <div className={classNames('messenger-preview-header-container')}>
+    <section>
+      <img alt="me" src={`${process.env.PUBLIC_URL}/avatars/000.jpeg`} />
+      <h3>聊天室</h3>
+      <button type="button">
+        <img
+          alt="setting"
+          src={`${process.env.PUBLIC_URL}/icons/setting.png`}
+        />
+      </button>
+      <button type="button">
+        <img alt="video" src={`${process.env.PUBLIC_URL}/icons/video.png`} />
+      </button>
+      <button type="button">
+        <img alt="write" src={`${process.env.PUBLIC_URL}/icons/write.png`} />
+      </button>
+    </section>
+    <input />
+  </div>
 );
 
 export default Header;
